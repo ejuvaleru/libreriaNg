1f90f8a0dc095fb98c3feb5a53c089c4d1e5b113
RodGpe <rodrigo.ch1995@gmail.com>
2020-01-27 00:21:07 -0600

    bussines logic mal hecha

--- src/app/components/agregar/agregar.component.ts
+++ src/app/components/agregar/agregar.component.ts
@@ -15,6 +15,7 @@ export class AgregarComponent implements OnInit {
   libroForm = this.fb.group({
     campoTitulo: ['', Validators.required],
     campoAutor: [''],
+    campoEditorial: [''],
     campoIsbn: [''],
     campoEdicion: [''],
     campoPaginas: [''],
@@ -22,6 +23,7 @@ export class AgregarComponent implements OnInit {
     campoDesc: [''],
     campoCostoCompra: [''],
     campoCostoVenta: [''],
+    campoCostoDescuento: [''],
     campoFecha: [''],
   });
 
@@ -34,6 +36,10 @@ export class AgregarComponent implements OnInit {
   libro;
   items: Resultado;
   cargando = false;
+  editoriales = [];
+  autores = [];
+  libros = [];
+  ID_LIBRO : number =0;
 
   constructor(
     private fb: FormBuilder,
@@ -45,7 +51,7 @@ export class AgregarComponent implements OnInit {
   ngOnInit() {
   }
 
-  onSubmit() {
+  async onSubmit() {
     // De esta manera accedemos al valor de todo el formulario
     // this.libroForm.value;
 
@@ -54,12 +60,107 @@ export class AgregarComponent implements OnInit {
     console.log(resultado.campoTitulo);
     // O así
     console.log(this.libroForm.get('campoAutor').value);
+    let existeLibro: boolean = false;
+    let existeAutor: boolean = false;
+    let existeEditorial: boolean = false;
+    const revisarCasos = async () => {
+      this.librosService.getEditoriales().subscribe(async l => {
+        this.editoriales = await l.data ;
+        //console.log(this.editoriales);
+        for (let editorial of this.editoriales) {
+          //console.log(editorial.nombre_editorial);
+          if (this.libro.editorial == editorial.nombre_editorial) {
+            console.log(this.libro.editorial + " es igual a " + editorial.nombre_editorial)
+            existeEditorial = true;
+          }
+        }
+      });
+      this.librosService.getAutores().subscribe(async l => {
+        this.autores = await l.data;
+        console.log(this.autores);
+        for (let autor of this.autores) {
+          if (this.libro.autor == autor.nombre_autor) {
+            console.log(this.libro.autor + " es igual a " + autor.nombre_autor)
+            existeAutor = true;
+          }
+        }
+      });
+      this.librosService.getLibros().subscribe(async l => {
+        this.libros =await l.data;
+        console.log(this.libros);
+        for (let libroRespuesta of this.libros) {
+          if (this.libro.isbn == libroRespuesta.isbn) {
+            console.log(this.libro.isbn + " es igual a " + libroRespuesta.isbn)
+            existeLibro = true;
+            this.ID_LIBRO = libroRespuesta.ID_libro;
+          }
+        }
+      });
+      console.log("esixre libro" , existeLibro);
+      if (existeLibro) {
+        //caso2 sí existe libro
+        let hoy = new Date();
+        let ejemplar = {
+          estado : this.libroForm.get('campoEstado'),
+          descripcion: this.libroForm.get('campoDesc').value,
+          costo_venta: this.libroForm.get('campoCostoVenta'),
+          costo_compra: this.libroForm.get('campoCostoCompra'),
+          costo_descuento: this.libroForm.get('campoCostoDescuento'),
+          url_fotografia: this.libro.portada,
+          fecha_adquisicion: hoy,
+          ID_LIBRO_libro: this.ID_LIBRO
+        };
+        this.librosService.insertarEjemplar(ejemplar).subscribe(l => {
+          console.log(l);
+          this.router.navigateByUrl('');
+        });
 
-    this.librosService.agregarLibro(resultado);
+      } else {//no existe libro
+        if (!existeAutor && !existeEditorial) {
+          //caso1
+        }
+        if (!existeAutor && existeEditorial) {
+          //caso3
+        }
+        if (existeEditorial && existeAutor) {
+          //caso4
+        }
+        if (existeAutor && !existeEditorial) {
+          //caso5
+        }
+      }
+
+    }
+    revisarCasos();
+
+    this.libro = {
+      titulo: this.libroForm.get('campoTitulo').value,
+      autor: this.libroForm.get('campoAutor').value,
+      editorial: this.libroForm.get('campoEditorial').value,
+      edicion: this.libroForm.get('campoEdicion').value,
+      descripcion: this.libroForm.get('campoDesc').value,
+      noPaginas: this.libroForm.get('campoPaginas').value,
+      isbn: this.libroForm.get('campoIsbn').value,
+      portada: this.libroForm.get('campoTitulo').value,
+    };
+    let libro2 = {
+      "num_pagina": 420,
+      "num_edicion": 1,
+      "isbn": "isbnFowler",
+      "codigo_identificador": null,
+      "titulo": "Qué es filosofia",
+      "EDITORIAL_ID_editorial": 1,
+      "NOMENCLATURA_ID_NOMENCLATURA": 1
+    }
+    //console.log(this.libro);
+
+    //this.librosService.insertarEjemplar(libro2).subscribe(l => {
+    //  console.log(l);
+    //});
 
     /* Utilizando el angular Router, podemos navegar entre páginas, así que es muy util para volver automáticamente
      cuando el formulario es llenado y enviado */
-    this.router.navigateByUrl('');
+    
   }
 
   async buscarPorIsbn() {
@@ -102,6 +203,7 @@ export class AgregarComponent implements OnInit {
     this.libroForm = this.fb.group({
       campoTitulo: [this.libro.titulo],
       campoAutor: [this.autor],
+      campoEditorial: [this.libro.editorial],
       campoIsbn: [this.isbnFromApi],
       campoEdicion: [''],
       campoPaginas: [this.libro.noPaginas],
@@ -109,6 +211,7 @@ export class AgregarComponent implements OnInit {
       campoDesc: [this.libro.descripcion],
       campoCostoCompra: [''],
       campoCostoVenta: [''],
+      campoCostoDescuento: [''],
       campoFecha: [''],
     });
   }
--- src/app/shared/books.service.ts
+++ src/app/shared/books.service.ts
@@ -17,10 +17,11 @@ export class BooksService {
   // TODO: más adelante aquí podríamos implementar los modelos definidos
   // Arreglo de libros o ejemplares
   libros = [];
-
   // Método para agregar, aquí sustituimos con una query
-  agregarLibro(libro) {
-    this.libros.push(libro);
+  insertarLibro(libro): Observable<any> {
+    //this.libros.push(libro);
+    console.log(libro);
+    return this.http.post(`${this.url}`, libro);
   }
 
   // Método de consulta, ya sea para editar o ver detalles, aquí sustituimos con una query
@@ -40,4 +41,46 @@ export class BooksService {
   getLibros(): Observable<any> {
     return this.http.get(`${this.url}`);
   }
+
+  getEditoriales(): Observable<any>{
+    return this.http.get(`http://localhost:4000/api/editoriales/`);
+  }
+
+  getAutores():  Observable<any>{
+    return this.http.get(`http://localhost:4000/api/autores/`);
+  }
+  insertarAutor(autor): Observable<any>{
+    //let autorNombre ={
+    //  "nombre_autor": "Luis "
+    //}
+    return this.http.post(`http://localhost:4000/api/autores/`,autor);
+  } 
+
+  insertarEditorial(editorial): Observable<any>{
+    let editorial2= {
+      "nombre_editorial": "The pragmatic bookshelf"
+    }
+    return this.http.post(`http://localhost:4000/api/editoriales/`,editorial2);
+  } 
+
+  insertarAutorLibro(autorLibro): Observable<any>{
+    //let autorLibro2= {
+    //  "LIBRO_ID_libro": 1,
+    //  "AUTOR_ID_autor": 3
+    //}
+    return this.http.post(`http://localhost:4000/api/autoresLibros/`,autorLibro);
+  } 
+  insertarEjemplar(ejemplar): Observable<any>{
+    //let autorLibro2= {
+    //  "estado": "usado",
+    //  "descripcion": "hojas rayadas",
+    //  "costo_venta": 100,
+    //  "costo_compra": 20,
+    //  "costo_descuento": 80,
+    //  "url_fotografia": "http://tuCorazzon",
+    //  "fecha_adquisicion": "2020-01-15",
+    //  "LIBRO_ID_libro": 1
+    //}
+    return this.http.post(`http://localhost:4000/api/ejemplares/`,ejemplar);
+  } 
 }
